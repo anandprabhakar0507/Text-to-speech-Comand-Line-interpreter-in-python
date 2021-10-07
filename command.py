95a2dacdbcf9ebfe3afd46d8b477a9f71ddb13a9
Anand prabhakar <anandprabhakar0507@gmail.com>
2021-10-07 23:11:39 +0530

    updated the interactive cli
    updated the software

--- command.py
+++ command.py
@@ -11,7 +11,7 @@ engine.say("Hello there..how are you..")
 engine.say("Please tell me your name by typing ")
 engine.runAndWait()
 user = input("Please Enter Your name : ")
-engine.say("Hello {0}".format(user))
+engine.say(f"Hello {user}")
 engine.runAndWait()
 
 
@@ -48,16 +48,18 @@ else:
 
 
 os.system("color 0e")
-
+# this funcction checks for the dependencies for running this program 
 def install(package):
     os.system("color 5f")
     engine = pyttsx3.init()
-    engine.say("keep working meanwhile i am checking your voice dependencies")
+    engine.say("let me check the voice dependencies before continuing")
     engine.runAndWait()
     if hasattr(pip, 'main'):
+        engine.say("I am installing the dependencies")
         pip.main(['install', package])
     else:
         pip._internal.main(['install', package])
+        engine.say("I am done now, it is ready now")
     engine.say("All dependencies are update and working")
     engine.runAndWait()
     
@@ -67,16 +69,18 @@ engine.say("This is a Modern command line Interface created by Anand Prabhakar")
 engine.runAndWait()
 
 print("==========================================================================")
-print("\n||\tCommand Line Interface CLI 1.0 By Anand Prabhakar,Bihar,India\t||\n||\tPowered by Python. \t\t\t\t\t\t||\n||\tjust learning..no commercial use.. \t\t\t\t||\n")
+print("\n||\tCommand Line Interface CLI 1.0 By Anand Prabhakar, Nalanda, Bihar,India\t||\n||\tPowered by Python. \t\t\t\t\t\t||\n||\tjust learning..no commercial use.. \t\t\t\t||\n")
 print("==========================================================================")
 def main():
     engine = pyttsx3.init()
     engine.say("Please Enter the command you want to run")
     engine.runAndWait()
+    #it takes commands as input
     command = input("\n\tEnter the command you want to execute : ")
     
     print("Output after execution : \n")
     print("============================================================")
+    #it runs the command and show output
     commanding(command)
     print("============================================================")
     
