4d498fd985f6bf1165be9142c3bf38ac98b99740
Restyled.io <commits@restyled.io>
2021-10-09 16:43:45 +0000

    Restyled by black

--- command.py
+++ command.py
@@ -11,27 +11,27 @@ engine.say("Hello {0}".format(user))
 engine.runAndWait()
 
 
-voices = engine.getProperty('voices')
+voices = engine.getProperty("voices")
 print(voices)
-print('these are available voices')
-engine.setProperty('voice', voices[1].id)
+print("these are available voices")
+engine.setProperty("voice", voices[1].id)
 
 
 engine.say("If you are a boy then simply type b and for girl please type g")
 engine.runAndWait()
 gender = input(str("please write here b for boy or g for girl : "))
 
-if(gender == "B" or gender == "b"):
-    voices = engine.getProperty('voices')
-    engine.setProperty('voice', voices[0].id)
+if gender == "B" or gender == "b":
+    voices = engine.getProperty("voices")
+    engine.setProperty("voice", voices[0].id)
     engine = pyttsx3.init()
     engine.say("Thank you you are ready to go..")
     engine.runAndWait()
     engine.stop()
 
-elif(gender == "G" or gender == "g"):
-    voices = engine.getProperty('voices')
-    engine.setProperty('voice', voices[1].id)
+elif gender == "G" or gender == "g":
+    voices = engine.getProperty("voices")
+    engine.setProperty("voice", voices[1].id)
     engine = pyttsx3.init()
     engine.say("Thank you you are ready to go..")
     engine.runAndWait()
@@ -53,10 +53,10 @@ def install(package):
     engine = pyttsx3.init()
     engine.say("keep working meanwhile i am checking your voice dependencies")
     engine.runAndWait()
-    if hasattr(pip, 'main'):
-        pip.main(['install', package])
+    if hasattr(pip, "main"):
+        pip.main(["install", package])
     else:
-        pip._internal.main(['install', package])
+        pip._internal.main(["install", package])
     engine.say("All dependencies are update and working")
     engine.runAndWait()
 
@@ -65,7 +65,9 @@ engine.say("This is a Modern command line Interface created by Anand Prabhakar")
 engine.runAndWait()
 
 print("==========================================================================")
-print("\n||\tCommand Line Interface CLI 1.0 By Anand Prabhakar,Bihar,India\t||\n||\tPowered by Python. \t\t\t\t\t\t||\n||\tjust learning..no commercial use.. \t\t\t\t||\n")
+print(
+    "\n||\tCommand Line Interface CLI 1.0 By Anand Prabhakar,Bihar,India\t||\n||\tPowered by Python. \t\t\t\t\t\t||\n||\tjust learning..no commercial use.. \t\t\t\t||\n"
+)
 print("==========================================================================")
 
 
@@ -99,5 +101,5 @@ def commanding(i):
 
 
 if __name__ == "__main__":
-    install('pyttsx3')
+    install("pyttsx3")
     main()
